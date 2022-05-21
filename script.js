8bb39fe7459b44d71afa18ff3b0b5b25d6147edd
theoklahuman <theoklahuman@gmail.com>
2022-05-21 14:00:39 -0500

    Create structure and display for calculator

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function divide(a, b) {
 function multiply(a, b) {
     return a * b;
 }
+
+function operate(a, b, operator) {
+    if (operator == add) {
+        return add(a, b);
+    } else if (operator == subtract) {
+        return subtract(a, b)
+    } else if (operator == multiply) {
+        return multiply(a, b);
+    } else if (operator == divide) {
+        return divide(a, b);
+    }
+}
