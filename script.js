03a2dbef631bbc061b09fff027072353ae3605e4
theoklahuman <theoklahuman@gmail.com>
2022-11-26 20:19:35 -0600

    Make calculator work better

--- script.js
+++ script.js
@@ -2,42 +2,13 @@ const display = document.querySelector("#display");
 const resultScreen = document.querySelector("#result-screen");
 
 const digits = document.querySelector(".digits");
-const multiplybtn = document.querySelector("#multiplication");
-const addbtn = document.querySelector("#addition");
-const subtractbtn = document.querySelector("#subtraction");
-const dividebtn = document.querySelector("#division");
+const operators = document.querySelector(".operators");
 
+let operator;
+let result;
 let newArray = [];
+let activeOperator = [];
 let numberOnDisplay;
-
-
-function showResult(value) {
-  resultScreen.innerHTML = value;
-}
-
-multiplybtn.addEventListener("click", function (event) {
-  newArray.push(multiply, numberOnDisplay);
-  numberOnDisplay = "";
-  display.innerHTML = "";
-});
-
-addbtn.addEventListener("click", function (event) {
-  newArray.push(add, numberOnDisplay);
-  numberOnDisplay = "";
-  display.innerHTML = "";
-});
-
-subtractbtn.addEventListener("click", function (event) {
-  newArray.push(subtract, numberOnDisplay);
-  numberOnDisplay = "";
-  display.innerHTML = "";
-});
-
-dividebtn.addEventListener("click", function (event) {
-  newArray.push(divide, numberOnDisplay);
-  numberOnDisplay = "";
-  display.innerHTML = "";
-});
 
 digits.addEventListener("click", function (event) {
   numberOnDisplay = +(display.innerHTML += `${event.target.innerHTML}`);
@@ -45,18 +16,53 @@ digits.addEventListener("click", function (event) {
   return numberOnDisplay;
 });
 
+
+operators.addEventListener("click", function(event) {
+  operator = event.target.id;
+  newArray.push(numberOnDisplay);
+  numberOnDisplay = 0;
+  display.innerHTML = "";
+
+  if (newArray.length === 1) {
+    activeOperator.push(operator);
+  } else {
+    let previousNumber = newArray[0];
+    let whatToOperate = activeOperator[0];
+    let currentNumber = newArray[1];
+    result = operate(whatToOperate, previousNumber, currentNumber);
+    resultScreen.innerHTML = result;
+    newArray.length = 0;
+    newArray.push(result);
+  activeOperator.length = 0;
+  activeOperator.push(operator);
+  }
+  
+});
+
+function operate(operator, num1, num2) {
+  if (operator === "add") {
+    return add(num1, num2);
+  } else if (operator == "subtract") {
+    return subtract(num1, num2);
+  } else if (operator == "multiply") {
+    return multiply(num1, num2);
+  } else if (operator == "divide") {
+    return divide(num1, num2);
+  }
+}
+
 function multiply(num1, num2) {
-  let result = num1 * num2;
+  result = num1 * num2;
   return result;
 }
 
 function add(num1, num2) {
-  let result = num1 + num2;
+  result = num1 + num2;
   return result;
 }
 
 function subtract(num1, num2) {
-  let result = num1 - num2;
+  result = num1 - num2;
   return result;
 }
 
@@ -64,37 +70,31 @@ function divide(num1, num2) {
   if (num2 == 0) {
     return "ERROR!";
   } else {
-    let result = num1 / num2;
+  result = num1 / num2;
     return result;
   }
 }
 
-function operate(operator, num1, num2) {
-  if (operator == add) {
-    return add(num1, num2);
-  } else if (operator == subtract) {
-    return subtract(num1, num2);
-  } else if (operator == multiply) {
-    return multiply(num1, num2);
-  } else if (operator == divide) {
-    return divide(num1, num2);
-  }
-}
-
-equal.addEventListener("click", function (event) {
-  newArray.push(numberOnDisplay);
-  let operator = newArray[0];
-  let num1 = newArray[1];
-  let num2 = newArray[2];
-  let totalResult = operate(operator, num1, num2);
-  showResult(totalResult);
-  display.innerHTML = "";
-});
-
-function defaultDisplay() {
+function clearDisplayScreen() {
   display.innerText = "0";
   resultScreen.innerHTML = "";
   newArray.length = 0;
+  activeOperator.length = 0;
 }
 
-clear.addEventListener("click", defaultDisplay);
+clear.addEventListener("click", clearDisplayScreen);
+
+
+equal.addEventListener("click", function (event) {
+  newArray.push(numberOnDisplay);
+  if (newArray.length <= 1 ) {
+    clearDisplayScreen();
+    return;
+  } else {
+    let previousNumber = newArray[0];
+    let whatToOperate = activeOperator[0];
+    let currentNumber = newArray[1];
+    result = operate(whatToOperate, previousNumber, currentNumber);
+    resultScreen.innerHTML = result;
+}
+})
